f19fc3cf5419b483ab35a10d38311df57d74521c
Sean Fisher <hi@seanfisher.co>
2014-12-14 19:48:20 -0500

    Search in place

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,9 +1,41 @@
 <?php
-
 class HomeController extends BaseController {
 	public function index()
 	{
-		return View::make('search');
+		$tweets = Es::getIndex('tweets');
+
+		// Build the query
+		$query = new Elastica\Query();
+
+		if (Input::has('q') && Input::get('q')) {
+			$elasticaQueryString = new Elastica\Query\QueryString;
+			$elasticaQueryString->setDefaultOperator('AND');
+			$elasticaQueryString->setQuery(Input::get('q', ''));
+			$query->setQuery($elasticaQueryString);
+		}
+
+		if (Input::has('user')) {
+			$userFilter = new Elastica\Filter\Term(['user' => Input::get('user')]);
+			$query->addFilter($userFilter);
+		}
+
+		$query->setSort([
+			['timestamp' => ['order' => 'desc']],
+			//'_score'
+		]);
+
+		try {
+			$results = $tweets->getType('tweet')->search($query);
+		} catch (Exception $e) {
+			Log::error($e);
+		}
+
+		return View::make('search')
+			->with('results', $results);
 	}
 
+	public function getSearch()
+	{
+
+	}
 }
---
+++ app/views/search.blade.php
@@ -0,0 +1,63 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Twitter for NJIT</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="/vendor/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+  <div class="container">
+    <h1 class="page-title">Twitter Search for <a href="http://twitter.com/njit">NJIT</a></h1>
+
+    <form action="/" method="get">
+      <input type="text" name="q" value="{{Input::get('q', '')}}" class="form-control input-lg" placeholder="Search for...">
+      <button type="submit" class="btn btn-primary">
+        Search
+      </button>
+      <br>
+      <br>
+    </form>
+
+    <table class="table table-striped">
+      <tbody>
+        @if ($results->count() < 1)
+        <tr>
+          <td>No results found.</td>
+        </tr>
+        @endif
+
+        @foreach($results as $result)
+        <tr>
+          <td>{{$result->message}}</td>
+
+          <td>
+            <a href="http://twitter.com/{{$result->user}}">{{'@'.$result->user}}</a>
+          </td>
+          <td>
+            <a href="http://twitter.com/{{$result->user}}/status/{{$result->id}}">
+              {{Carbon\Carbon::parse($result->timestamp)->diffForHumans()}}
+            </a>
+          </td>
+
+        </tr>
+
+        @endforeach
+    </table>
+  </div>
+</body>
+</html>
